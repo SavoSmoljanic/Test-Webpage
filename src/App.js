bdc670415766abf5b3ce18e133cfe5f9cd529b39
savo smoljanic <salvation123123@gmail.com>
2023-04-04 12:14:16 +1000

    created a header componant

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import { Header } from "./Componants/Header/Header";
 function App() {
   return (
     <div className="App">
-      <p>hello world</p>
+      <Header />
     </div>
   );
 }
---
+++ src/Componants/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+export function Header() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Product</li>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
