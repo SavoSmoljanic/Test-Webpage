517e9950b9a3648308de4be11eb0b5a1494a8523
savo smoljanic <salvation123123@gmail.com>
2023-04-04 14:00:36 +1000

    created main h1 and h4 elements along with button element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Header } from "./Componants/Header/Header";
+import { Title } from "./Componants/Title/Title";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Title />
     </div>
   );
 }
---
+++ src/Componants/Title/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Title.css";
+export function Title() {
+  return (
+    <div>
+      <h1 className="heading">The best products start with figma</h1>
+      <h4 className="subHeading">
+        Most calendars are designed for teams. Slate is designed for freelancers
+      </h4>
+      <div className="buttonContainer">
+        <button>Try For Free</button>
+      </div>
+    </div>
+  );
+}
