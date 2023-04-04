13309f59f38f743e21825e077f8af918d2747c26
savo smoljanic <salvation123123@gmail.com>
2023-04-05 08:57:52 +1000

    added background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Header } from "./Componants/Header/Header";
 import { Title } from "./Componants/Title/Title";
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Title />
+      <div className="bgImage">
+        <Header />
+        <Title />
+      </div>
     </div>
   );
 }
